b70b66a7634a71857422c8cb34e533b3dc6e09fc
kofany <92736603+kofany@users.noreply.github.com>
2023-10-31 07:57:30 +0100

    Update action.h

--- action.h
+++ action.h
@@ -1,7 +1,7 @@
 #ifndef __ACTION_H
 #define __ACTION_H
 
-#define MAX_PARSE	20 // tyle +o maxymalnie parsujemy
+#define MAX_PARSE	20 // tyle +o maxymalnie parsujemyy
 
 extern xchan *parse_act, *parse_mode_chan;
 extern int parse_op;
