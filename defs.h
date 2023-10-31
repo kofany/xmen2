50ae525320e63f2af4ffb11f135e182c790891ed
kofany <92736603+kofany@users.noreply.github.com>
2023-10-31 08:12:01 +0100

    Update defs.h

--- defs.h
+++ defs.h
@@ -21,74 +21,75 @@
 
 #define xdebug(l, c...) {			\
 	if (l <= debuglvl) {			\
-	write(1, COLOR_BLACK, COLOR_LEN);	\
-	write(1, "[", 1);			\
-	write(1, COLOR_NORMAL, COLOR_NORLEN);	\
-	write(1, "*", 1);			\
-	write(1, COLOR_BLACK, COLOR_LEN);	\
-	write(1, "] ", 2);			\
-	write(1, COLOR_NORMAL, COLOR_NORLEN);	\
+	(void)write(1, COLOR_BLACK, COLOR_LEN);	\
+	(void)write(1, "[", 1);			\
+	(void)write(1, COLOR_NORMAL, COLOR_NORLEN);	\
+	(void)write(1, "*", 1);			\
+	(void)write(1, COLOR_BLACK, COLOR_LEN);	\
+	(void)write(1, "] ", 2);			\
+	(void)write(1, COLOR_NORMAL, COLOR_NORLEN);	\
 	printf(c); } }
 
 #define info_printf(c...) { 			\
-	write(1, COLOR_BLACK, COLOR_LEN); 	\
-	write(1, "[", 1);			\
-	write(1, COLOR_CYAN, COLOR_LEN);	\
-	write(1, "*", 1);			\
-	write(1, COLOR_BLACK, COLOR_LEN);	\
-	write(1, "] ", 2);			\
-	write(1, COLOR_NORMAL, COLOR_NORLEN);	\
+	(void)write(1, COLOR_BLACK, COLOR_LEN); 	\
+	(void)write(1, "[", 1);			\
+	(void)write(1, COLOR_CYAN, COLOR_LEN);	\
+	(void)write(1, "*", 1);			\
+	(void)write(1, COLOR_BLACK, COLOR_LEN);	\
+	(void)write(1, "] ", 2);			\
+	(void)write(1, COLOR_NORMAL, COLOR_NORLEN);	\
 	printf(c); }
 
 #define cdel_printf(c...) { 			\
-	write(1, COLOR_BLACK, COLOR_LEN); 	\
-	write(1, "[", 1);			\
-	write(1, COLOR_LCYAN, COLOR_LEN);	\
-	write(1, "-", 1);			\
-	write(1, COLOR_BLACK, COLOR_LEN);	\
-	write(1, "] ", 2);			\
-	write(1, COLOR_NORMAL, COLOR_NORLEN);	\
+	(void)write(1, COLOR_BLACK, COLOR_LEN); 	\
+	(void)write(1, "[", 1);			\
+	(void)write(1, COLOR_LCYAN, COLOR_LEN);	\
+	(void)write(1, "-", 1);			\
+	(void)write(1, COLOR_BLACK, COLOR_LEN);	\
+	(void)write(1, "] ", 2);			\
+	(void)write(1, COLOR_NORMAL, COLOR_NORLEN);	\
 	printf(c); }
 
 #define cadd_printf(c...) { 			\
-	write(1, COLOR_BLACK, COLOR_LEN); 	\
-	write(1, "[", 1);			\
-	write(1, COLOR_BLUE, COLOR_LEN);	\
-	write(1, "+", 1);			\
-	write(1, COLOR_BLACK, COLOR_LEN);	\
-	write(1, "] ", 2);			\
-	write(1, COLOR_NORMAL, COLOR_NORLEN);	\
+	(void)write(1, COLOR_BLACK, COLOR_LEN); 	\
+	(void)write(1, "[", 1);			\
+	(void)write(1, COLOR_BLUE, COLOR_LEN);	\
+	(void)write(1, "+", 1);			\
+	(void)write(1, COLOR_BLACK, COLOR_LEN);	\
+	(void)write(1, "] ", 2);			\
+	(void)write(1, COLOR_NORMAL, COLOR_NORLEN);	\
 	printf(c); }
 
 #define cinfo_printf(c...) { 			\
-	write(1, COLOR_BLACK, COLOR_LEN); 	\
-	write(1, "[", 1);			\
-	write(1, COLOR_GREEN, COLOR_LEN);	\
-	write(1, "*", 1);			\
-	write(1, COLOR_BLACK, COLOR_LEN);	\
-	write(1, "] ", 2);			\
-	write(1, COLOR_NORMAL, COLOR_NORLEN);	\
+	(void)write(1, COLOR_BLACK, COLOR_LEN); 	\
+	(void)write(1, "[", 1);			\
+	(void)write(1, COLOR_GREEN, COLOR_LEN);	\
+	(void)write(1, "*", 1);			\
+	(void)write(1, COLOR_BLACK, COLOR_LEN);	\
+	(void)write(1, "] ", 2);			\
+	(void)write(1, COLOR_NORMAL, COLOR_NORLEN);	\
 	printf(c); }
-	
+
 #define help_printf(c...) { 			\
-	write(1, COLOR_BLACK, COLOR_LEN); 	\
-	write(1, "[", 1);			\
-	write(1, COLOR_WHITE, COLOR_LEN);	\
-	write(1, "?", 1);			\
-	write(1, COLOR_BLACK, COLOR_LEN);	\
-	write(1, "] ", 2);			\
-	write(1, COLOR_NORMAL, COLOR_NORLEN);	\
+	(void)write(1, COLOR_BLACK, COLOR_LEN); 	\
+	(void)write(1, "[", 1);			\
+	(void)write(1, COLOR_WHITE, COLOR_LEN);	\
+	(void)write(1, "?", 1);			\
+	(void)write(1, COLOR_BLACK, COLOR_LEN);	\
+	(void)write(1, "] ", 2);			\
+	(void)write(1, COLOR_NORMAL, COLOR_NORLEN);	\
 	printf(c); }
 
 #define err_printf(c...) { 			\
-	write(1, COLOR_BLACK, COLOR_LEN); 	\
-	write(1, "[", 1);			\
-	write(1, COLOR_RED, COLOR_LEN);		\
-	write(1, "!", 1);			\
-	write(1, COLOR_BLACK, COLOR_LEN);	\
-	write(1, "] ", 2);			\
-	write(1, COLOR_NORMAL, COLOR_NORLEN);	\
+	(void)write(1, COLOR_BLACK, COLOR_LEN); 	\
+	(void)write(1, "[", 1);			\
+	(void)write(1, COLOR_RED, COLOR_LEN);		\
+	(void)write(1, "!", 1);			\
+	(void)write(1, COLOR_BLACK, COLOR_LEN);	\
+	(void)write(1, "] ", 2);			\
+	(void)write(1, COLOR_NORMAL, COLOR_NORLEN);	\
 	printf(c); }
+
 
 typedef struct clone_t xmen; // clones.h
 // irc.h
